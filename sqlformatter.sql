fff5d38cce2591d126e08d90612f417ea413c11e
mrabets <newrabgo@gmail.com>
2021-11-27 15:04:54 +0300

    Add sql script

---
+++ sqlformatter.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE db_utest;
+
+USE db_utest;
+
+CREATE TABLE user (
+  id INT PRIMARY KEY AUTO_INCREMENT, 
+  login VARCHAR(40) NOT NULL UNIQUE, 
+  password VARCHAR(255) NOT NULL, 
+  is_admin TINYINT(1) DEFAULT 0
+);
+
+CREATE TABLE unit_tests (
+  id INT PRIMARY KEY AUTO_INCREMENT, 
+  name VARCHAR(50), 
+  errors_number INT, 
+  lead_time TIME, 
+  created_at DATE
+);
+
+CREATE TABLE test_cases (
+  id INT PRIMARY KEY AUTO_INCREMENT, 
+  name VARCHAR(80), 
+  last_launch DATETIME, 
+  is_complete TINYINT(1) DEFAULT 0, 
+  framework VARCHAR(40)
+);
